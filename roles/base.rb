d8e06a28720a0019cc1d61e85535043555f0b4c1
Arafat <arfat619@gmail.com>
2016-10-18 03:50:48 +0000

    Updating Chef-Repo

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name "base"
 description "contains recipes that should be on all ndoes"
-run_list "recipe[security]", "recipe[localusers]"
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client::cron]", "recipe[chef-client]", "recipe[localusers]"
